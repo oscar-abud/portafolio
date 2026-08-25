f1a0fc523cff5801dc8740cf319105d581dae56b
oscar-abud <oscarpalma.vpr2@gmail.com>
2026-08-24 23:04:01 -0400

    [FIX] Fix tailwind configuration

--- tailwind.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: 'selector', // 👈 Agrega esto para el modo oscuro/claro
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
